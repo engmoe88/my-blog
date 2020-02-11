320219bb949f95175c12cb31d6971a426d03f384
Mohammed Almukhtar <msalmukhtar@sbgtv.com>
2020-02-10 18:11:13 -0800

    first commit

---
+++ src/build/precache-manifest.21e0eb918ec6a68d125e5f7911b094ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025552baafe6f4235d92888a0320bc1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23277cfd64ba0decce62",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "7c5c7c28c89d521ceeda",
+    "url": "/static/js/2.ea58dc3f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ea58dc3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23277cfd64ba0decce62",
+    "url": "/static/js/main.0c3bb572.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
